6039c8892265dcde0a7dc9937bdaa9dcb2866f6c
dewagedeagung <dewagede.septiyadi@ist.id>
2022-06-01 22:34:22 +0700

    Developed Service API Register and Login

--- config.js
+++ config.js
@@ -11,5 +11,6 @@ module.exports = {
   serverConfig: {
       ip: config.serverIp,
       port: config.serverPort
-  }
+  },
+  tokenScret: "foodelivery_secret"
 };
---
+++ routes/authentication.js
@@ -0,0 +1,20 @@
+var express = require('express');
+const { response } = require('../app');
+const { userRegister, userLogin } = require('../services/authentication.service');
+var router = express.Router();
+
+
+
+router.post('/register', async (req, res, next) => {
+  let body = req.body;
+  let response = await userRegister(body)
+  res.json(response)
+});
+
+router.post('/login', async (req, res, next) => {
+  let body = req.body;
+  let response = await userLogin(body)
+  res.json(response)
+});
+
+module.exports = router;
